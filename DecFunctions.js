a7a6658d3319cf407a864ab9946f800fd745e0b3
Lucy Ly <lucyly.student@careerdevs.com>
2019-06-01 17:18:51 +0000

    Write Concise Declarative Functions

---
+++ DecFunctions.js
@@ -0,0 +1,24 @@
+// // change code below this line
+// const bicycle = {
+//   gear: 2,
+//   setGear: function(newGear) {
+//     "use strict";
+//     this.gear = newGear;
+//   }
+// };
+// // change code above this line
+// bicycle.setGear(3);
+// console.log(bicycle.gear);
+
+
+// change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        "use strict";
+        this.gear = newGear;
+    }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
